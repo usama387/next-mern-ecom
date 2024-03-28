7440cf6d71ab0d090a994427b7b299b4bcb56200
usama387 <128306049+usama387@users.noreply.github.com>
2024-03-28 19:17:44 +0500

    Added basic project config and clerk auth

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,13 @@
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <UserButton />
+      HomePage
+    </div>
+  );
+};
+
+export default HomePage;
