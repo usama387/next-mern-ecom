9cf3a28f5e085e407e78910a16524f4de1fdfbf0
usama387 <128306049+usama387@users.noreply.github.com>
2024-03-28 21:14:06 +0500

    Changed background color of sign-in and sign-up pages

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const Sign = () => {
   return (
-    <div className="h-screen flex justify-center items-center bg-slate-400">
+    <div className="h-screen flex justify-center items-center bg-slate-900">
       <SignUp />
     </div>
   );
